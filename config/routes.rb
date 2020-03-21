3b9d2a56f56d87a26af4cd7043a55903d9c7e883
DAITADOKORO <bangasa0101@gmail.com>
2020-03-21 17:30:32 +0900

    add bootstrap3

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'trips/index'
+  get 'trips/show'
+  get 'users/show'
+  get 'users/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

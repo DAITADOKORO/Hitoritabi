35fd1304853439b51f622a45f44f99281c3e05d3
DAITADOKORO <bangasa0101@gmail.com>
2020-03-23 17:08:29 +0900

    add smallmap-js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,73 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+
+
+$(function () {
+    $('.tohoku').click(function () {
+        $('.smallmap').show();
+        $('#tohokumap').show();
+        $('.bigmap').hide();
+    });
+});
+
+$(function () {
+    $('.kanto').click(function () {
+        $('.smallmap').show();
+        $('#kantomap').show();
+        $('.bigmap').hide();
+    });
+});
+
+$(function () {
+    $('.hokuriku').click(function () {
+        $('.smallmap').show();
+        $('#hokurikumap').show();
+        $('.bigmap').hide();
+    });
+});
+$(function () {
+    $('.tyubu').click(function () {
+        $('.smallmap').show();
+        $('#tyubumap').show();
+        $('.bigmap').hide();
+    });
+});
+$(function () {
+    $('.kansai').click(function () {
+        $('.smallmap').show();
+        $('#kansaimap').show();
+        $('.bigmap').hide();
+    });
+});
+$(function () {
+    $('.shikoku').click(function () {
+        $('.smallmap').show();
+        $('#shikokumap').show();
+        $('.bigmap').hide();
+    });
+});
+
+$(function () {
+    $('.tyugoku').click(function () {
+        $('.smallmap').show();
+        $('#tyugokumap').show();
+        $('.bigmap').hide();
+    });
+});
+
+$(function () {
+    $('.').click(function () {
+        $('.smallmap').show();
+        $('#map').show();
+        $('.bigmap').hide();
+    });
+});
+
+$(function () {
+    $('.kyusyu').click(function () {
+        $('.smallmap').show();
+        $('#kyusyumap').show();
+        $('.bigmap').hide();
+    });
+});
